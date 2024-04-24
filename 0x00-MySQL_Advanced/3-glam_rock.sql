3819ee780efcefc896fa55771a2c425a2e9e5408
AhmedEhab2022 <am8122004@gmail.com>
2024-04-24 22:58:28 +0200

    3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,6 @@
 -- SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
 
+-- Select band_name and lifespan from bands
 SELECT band_name, (LEAST(2022, COALESCE(split, 2022)) - formed) AS lifespan
 FROM bands
 WHERE style = 'Glam rock' AND formed <= 2022
