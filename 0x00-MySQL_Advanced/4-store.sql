808f6c9340c873a0f865d4b9a86f5b0912cb7ea8
AhmedEhab2022 <am8122004@gmail.com>
2024-04-25 00:08:52 +0200

    4-store.sql

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -6,6 +6,6 @@ AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
   UPDATE items
-  SET quantity = quantity - orders.number
+  SET quantity = quantity - NEW.number
   WHERE items.name = NEW.item_name
 END;
