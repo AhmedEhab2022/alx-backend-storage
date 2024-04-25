d2486e425e78f5d7a682d44b4080e50d0efc8650
AhmedEhab2022 <am8122004@gmail.com>
2024-04-25 17:51:31 +0200

    101-average_weighted_score.sql

---
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,21 @@
+-- SQL script that creates a stored procedure ComputeAverageWeightedScoreForUsers that
+-- computes and store the average weighted score for all students.
+
+-- Custom delimiter
+DELIMITER $$
+
+-- Create stored procedure
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+BEGIN
+
+  -- Update the average score
+  UPDATE users
+  SET average_score = (SELECT (SUM(weight * score) / Sum(weight))
+                      FROM corrections, projects, users
+                      WHERE corrections.user_id = users.id
+                      AND projects.id = project_id);
+
+END$$
+
+-- Reset delimiter
+DELIMITER ;
