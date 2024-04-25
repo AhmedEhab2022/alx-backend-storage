e0216cf57ad66ab515e2438fe7bee1e000448e1e
AhmedEhab2022 <am8122004@gmail.com>
2024-04-25 17:07:32 +0200

    3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,6 +2,6 @@
 
 -- Select band_name and lifespan from bands
 SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
-FROM bands
+FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
