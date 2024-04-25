fc7c6dc77195a9f1ce251277a083ad0a6c05bf27
AhmedEhab2022 <am8122004@gmail.com>
2024-04-25 14:34:58 +0200

    7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -13,7 +13,7 @@ BEGIN
   UPDATE users
   SET average_score = (SELECT AVG(score)
                       FROM corrections
-                      WHERE projects.user_id = user_id)
+                      WHERE corrections.user_id = user_id)
   WHERE id = user_id;
 
 END$$
