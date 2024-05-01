f49955491312c3ce181c19a24feb0318a159a0bb
AhmedEhab2022 <am8122004@gmail.com>
2024-05-01 20:32:08 +0300

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,13 +4,29 @@ This module contains get_page function
 """
 import requests
 import redis
+from typing import Callable
+from functools import wraps
 
+r = redis.Redis()
 
+def cache_page(func: Callable) -> Callable:
+    """
+    Cache the result of the get_page function in Redis
+    """
+    @wraps(func)
+    def wrapper(url):
+        """Wrapper function"""
+        key = f"count:{url}"
+        if r.exists(key):
+            return r.get(key)
+        else:
+            result = func(url)
+            r.set(key, result, ex=10)
+            return result
+        return wrapper
+
+@cache_page    
 def get_page(url: str) -> str:
     """Get the content of a web page."""
-    r = redis.Redis()
-    key = f"count:{url}"
-    r.incr(key)
-    r.expire(key, 10)
     response = requests.get(url)
     return response.text
